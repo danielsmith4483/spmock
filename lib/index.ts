33695246c593e931ca431d452e27e10805d3126a
Daniel Smith <danielsmith4483@gmail.com>
2018-07-24 22:02:46 -0500

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,15 @@
+export interface IOptions {
+
+}
+
+/**
+ * Returns a spongemock version of a given string.
+ * @param inputString The raw input string to spongemockify.
+ * @param options Optional parameter conforming to IOptions for supplying additional customization.
+ * @returns The spongemockified string.
+ */
+export function spmock(inputString: string, options?: IOptions): string {
+    return inputString.split('').map((el, i) => {
+        return i % 2 == 0 ? el.toLowerCase() : el.toUpperCase();
+    }).join('');
+}
